4800493911a5ecb1b67ba88eb54f6f720dcc8ea5
JosephJBassey <136816366+Josephjbassey@users.noreply.github.com>
2023-08-31 14:59:03 +0100

    feat: Add Bash script for compiling C source files
    This adds a Bash script that automates the compilation of C source files.
    The script takes a filename as an argument and checks if the provided file has a '.c' extension.
    If so, it uses the 'gcc' compiler to generate an executable with the same name (without the '.c' extension).
    If the file is not a C source file, the script exits with an error message.
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # Date:      Thu Aug 31 14:11:29 2023 +0100
    #
    # On branch main
    # Your branch is up to date with 'origin/main'.
    #
    # Changes to be committed:
    #	new file:   c_compiler.sh
    #
    # Untracked files:
    #	script.sh
    #

---
+++ script.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+filename=$1
+
+# create a new file with touch
+touch $filename
+
+if [ ! -s $filename ]; then
+echo "File is empty. Please add content to file before proceeding."
+read -p "Do you want to add content to the file now? (y/n): " choice
+
+if [ "$choice" = "y" ]; then
+echo "Please select a text editor to add content to file:"
+select text_editor in "vi" "emacs" "nano" "nvim"
+do
+	case $text_editor in 
+		vi )
+			vi $filename
+			echo "File content added using vi editor."
+			break
+			;;
+		emacs )
+			emacs $filename
+			echo "File content added using emacs editor."
+			break
+			;;
+		nano )
+			nano $filename
+			echo "File content added using nano editor."
+			break
+			;;
+		nvim )
+			nvim $filename
+			echo "File content added using neovim editor."
+			break
+			;;
+
+		*)
+			echo "Invalid option. Please select a valid text editor."
+			esac
+	done
+else
+		echo "Aborting script."
+		exit
+fi
+
+else
+read -p "Do you want to edit the file? (y/n):" edit_choice
+if [ "$edit_choice" = "y" ]; then
+echo "Please select a text editor to add content to file:"
+select text_editor in "vi" "emacs" "nano" "nvim"
+do
+	case $text_editor in
+		vi )
+			vi $filename
+			echo "File content added using vi editor."
+			break
+			;;
+		emacs)
+			emacs $filename
+			echo "File content added using emacs editor."
+			break
+			;;
+		nano )
+			nano $file
+			echo "File content added using nano editor."
+			break
+			;;
+		nvim )
+			nvim $filename
+			echo "File content added using neovim editor."
+			break
+			;;
+		*)
+			echo "Invalid option. Please select a valid text editor."
+	esac
+done
+
+fi
+fi
+
+# change the execute permission for the owner of the file
+read -p "Do you want to set permissions to file? (y/n): " exe_choice
+if [ "$exe_choice" = "y" ]; then
+read -p "Do you want to set file permissions numerical value? (y/n):" perm_choice
+
+	if [ "$perm_choice" = "y" ]; then
+		read -p "Enter the permission value (e.g. 755):" perm_value
+		chmod $perm_value $filename
+		echo "File permissions set to $perm_value"
+	else
+		chmod u+x $filename
+		echo "File permissions set to u+x for owner"
+	fi
+fi
+
+# Get the root directory of the Git repository
+repo_root=$(git rev-parse --show-toplevel)
+
+# add the file to git
+read -p "Do you want to push changes to remote repository? (y/n):" push_choice
+if  [ "$push_choice" = "y" ]; then
+	git add $filename
+
+# prompt the user for a commit message
+#
+echo "Enter a commit message"
+$text_editor "$repo_root/.git/COMMIT_EDITMSG"
+
+# commit the changes to the local repository
+git commit -F "$repo_root/.git/COMMIT_EDITMSG"
+
+# push the changes to the remote repository
+git push
+
+# Task Completed
+echo "Changes pushed to remote repository"
+echo "Task Completed Successfully, Good Job!!!"
+fi
+sleep 1
+echo "Good bye $(git config user.name)"
